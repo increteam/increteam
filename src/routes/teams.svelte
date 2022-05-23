d286029a2f72c1cc846e6175b381fcdb301178d6
AngelNext <angelgz.vazquez@gmail.com>
2022-05-23 17:13:09 +0000

    Made some breaking changes

--- src/routes/teams.svelte
+++ src/routes/teams.svelte
@@ -1,4 +1,4 @@
-<main class="p-5 rounded-md m-auto w-96 my-10 text-center text-indigo-600 border border-indigo-600">
+<main class="p-5 rounded-md m-auto w-96 my-10 text-center text-indigo-600 border-4 border-indigo-600">
 	<h1 class="font-title text-4xl md:text-6xl uppercase megawide">teams</h1>
 </main>
 
@@ -11,7 +11,7 @@
 				alt="Logo"
 			/>
 			<div class="p-6">
-				<h5 class="text-gray-900 text-xl font-medium mb-2">1ºC</h5>
+				<h5 class="text-gray-900 text-3xl font-bold mb-2">1ºC</h5>
 				<p class="text-gray-700 text-base mb-4">Este equipo se centra en la clase de 1ºC ESO.</p>
 			</div>
 		</div>
